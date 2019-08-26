435edf60d97de44dfec53d59787c678112ffa0eb
Mingxing Qian <mxqian2000@yahoo.com>
2019-08-26 07:46:20 -0500

    part 02

---
+++ cheese-mvc00/src/main/java/org/launchcode/cheesemvc00/CheeseMvc00Application.java
@@ -0,0 +1,13 @@
+package org.launchcode.cheesemvc00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseMvc00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeseMvc00Application.class, args);
+	}
+
+}
